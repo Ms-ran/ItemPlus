ae4b795bbc816ed993776d508f4f6907a8b073e0
Jerry <jerryhuang98@yahoo.com>
2015-05-21 06:00:45 -0400

    更改属性基础内容

--- src/com/ItemPlus/Attribute/Attribute.java
+++ src/com/ItemPlus/Attribute/Attribute.java
@@ -1,5 +1,8 @@
 package com.ItemPlus.Attribute;
 
+import java.util.UUID;
+import javax.annotation.Nonnull;
+
 /**
  * @author HotFlow
  */
@@ -11,4 +14,61 @@ public interface Attribute
      * @return
      */
     public String getName();
+
+    /**
+     * 设置名字
+     *
+     * @param name
+     */
+    public void setName(String name);
+
+    /**
+     * 获取类型
+     *
+     * @return
+     */
+    public Attribute getType();
+
+    /**
+     * 获取操作方式
+     *
+     * @return
+     */
+    public int getOperation();
+
+    /**
+     * 设置操作方式
+     * 增加数值(0)/乘以百分比(1)/增加百分比(2)
+     *
+     * @param operation
+     */
+    public void setOperation(int operation);
+
+    /**
+     * 获取数据
+     *
+     * @return
+     */
+    public Object getAmount();
+
+    /**
+     * 设置数据
+     *
+     * @param amount
+     */
+    public void setAmount(Object amount);
+
+    /**
+     * 获取UUID
+     *
+     * @return
+     */
+    public UUID getUUID();
+
+    /**
+     * 设置UUID
+     *
+     * @param id
+     */
+    public void setUUID(@Nonnull UUID id);
 }
