9c3eea5900ace1dc059bf79f4bec4a2564e3dde9
Jerry <jerryhuang98@yahoo.com>
2015-05-21 06:01:24 -0400

    修正属性内容

--- src/com/ItemPlus/Core/v0_0_1/Attribute/LifeSteal.java
+++ src/com/ItemPlus/Core/v0_0_1/Attribute/LifeSteal.java
@@ -1,15 +1,68 @@
 package com.ItemPlus.Core.v0_0_1.Attribute;
 
 import com.ItemPlus.Attribute.Attribute;
+import java.util.UUID;
 
 /**
  * @author HotFlow
  */
 public class LifeSteal implements Attribute
 {
+    private String name;
+    private int operation;
+    private int amount;
+    private UUID uuid;
+
+    public LifeSteal(String name, int operation, int amount, UUID uuid)
+    {
+        this.name = name;
+        this.operation = operation;
+        this.amount = amount;
+        this.uuid = uuid;
+    }
+
     public String getName()
     {
-        return "itemPlus.lifeSteal";
+        return this.name;
     }
-   
+
+    public void setName(String name)
+    {
+        this.name = name;
+    }
+
+    public Attribute getType()
+    {
+        return this;
+    }
+
+    public int getOperation()
+    {
+        return this.operation;
+    }
+
+    public void setOperation(int operation)
+    {
+        this.operation = operation;
+    }
+
+    public Integer getAmount()
+    {
+        return this.amount;
+    }
+
+    public void setAmount(Object amount)
+    {
+        this.amount = (Integer) amount;
+    }
+
+    public UUID getUUID()
+    {
+        return this.uuid;
+    }
+
+    public void setUUID(UUID id)
+    {
+        this.uuid = id;
+    }
 }
